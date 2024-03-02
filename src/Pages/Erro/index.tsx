9deef17297d4b4341cde4c9a13a8c60eacc8b857
dev-herbert <soyzabeto@gmail.com>
2024-03-02 16:26:19 -0300

    created page erro

--- src/Pages/Erro/index.tsx
+++ src/Pages/Erro/index.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 
 
 export const Erro = () => {
 
     return(
-        <h1>ola</h1>
+        <main>
+            <section className="flex items-center justify-center gap-2 flex-col min-h-screen">
+                <h1 className="font-bold text-2xl">Essa pagina não existe</h1>
+                <p className="text-base">Volte para pagina principal</p>
+                <Link to="/" className="bg-green-700 text-white font-bold text-base px-4 py-2 rounded-lg">Voltar</Link>
+            </section>
+        </main>
     );
 }
